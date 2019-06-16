94c18f1d361203f8f20615a8b3d0e8f2305d6802
Lucyna Mazur <lucyna@studiokreator.com>
2019-06-16 10:11:15 +0200

    remove unnecessary imports

--- photoalbum/models.py
+++ photoalbum/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-from django.core.files.uploadedfile import InMemoryUploadedFile
-from PIL import Image
-from io import BytesIO
-from django.core.files.base import ContentFile
 
 class MyUser(AbstractUser):
     email = models.EmailField(unique=True)
